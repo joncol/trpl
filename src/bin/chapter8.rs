75b2df9dd34fea9506265f7cb5337cb7ba0157c6
Jonas Collberg <jonas.collberg@gmail.com>
2020-08-01 14:09:33 +0200

    Run `cargo fmt` on `chapter8.rs`

--- src/bin/chapter8.rs
+++ src/bin/chapter8.rs
@@ -1,8 +1,7 @@
 use std::collections::HashMap;
 
 fn main() {
-    let (mean, median, mode) =
-        mean_median_mode(vec![5, 9, 1, 4, 7, 2, 7, 3, 4, 7]);
+    let (mean, median, mode) = mean_median_mode(vec![5, 9, 1, 4, 7, 2, 7, 3, 4, 7]);
 
     println!("Mean is: {}", mean);
     println!("Median is: {}", median);
@@ -22,22 +21,18 @@ fn mean_median_mode(v: Vec<i32>) -> (f32, f32, i32) {
     let mut sorted: Vec<i32> = v.to_vec();
     sorted.sort();
     let mid = sorted.len() / 2;
-    let median =
-        if sorted.len() % 2 == 0 {
-            ((sorted[mid - 1] + sorted[mid]) as f32) / 2.0
-        } else {
-            sorted[mid] as f32
-        };
+    let median = if sorted.len() % 2 == 0 {
+        ((sorted[mid - 1] + sorted[mid]) as f32) / 2.0
+    } else {
+        sorted[mid] as f32
+    };
     let mut map: HashMap<i32, i32> = HashMap::new();
     for x in v.iter() {
         let count = map.entry(*x).or_insert(0);
         *count += 1;
     }
 
-    let mode = map
-        .iter()
-        .max_by(|a, b| a.1.cmp(&b.1))
-        .map(|(k, _v)| k);
+    let mode = map.iter().max_by(|a, b| a.1.cmp(&b.1)).map(|(k, _v)| k);
 
     (mean, median, *mode.unwrap())
 }
@@ -47,7 +42,8 @@ fn pig_latin(s: &str) -> String {
         format!("{}-hay", s)
     } else {
         let first_char = s.chars().next().unwrap();
-        let but_first = s.chars()
+        let but_first = s
+            .chars()
             .next()
             .map(|c| &s[c.len_utf8()..])
             .unwrap()
