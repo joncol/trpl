8fef91e5f0ec865d4df5e36e1215025d2d7a2618
Jonas Collberg <jonas.collberg@gmail.com>
2020-08-01 22:35:56 +0200

    Improve user-data model

--- src/bin/chapter8_departments.rs
+++ src/bin/chapter8_departments.rs
@@ -5,9 +5,13 @@ use std::collections::HashMap;
 
 fn main() {
     let mut siv = cursive::default();
-
     siv.add_global_callback('q', Cursive::quit);
+    siv.set_user_data(create_user_data());
+    create_departments_view(&mut siv);
+    siv.run();
+}
 
+fn create_user_data() -> HashMap<String, Vec<String>> {
     let mut departments: HashMap<String, Vec<String>> = HashMap::new();
     departments.insert(
         String::from("Engineering"),
@@ -23,16 +27,17 @@ fn main() {
             .map(|s: &&str| s.to_string())
             .collect(),
     );
+    departments
+}
 
-    siv.set_user_data(departments);
-
+fn create_departments_view(s: &mut Cursive) {
     let department_buttons = LinearLayout::vertical()
         .child(Button::new("Create new", create_department))
         .child(Button::new("Remove", remove_department));
 
-    let departments_view = create_departments_view(&mut siv);
+    let departments_view = create_departments_list(s);
 
-    siv.add_layer(
+    s.add_layer(
         Dialog::around(
             LinearLayout::horizontal()
                 .child(departments_view)
@@ -42,14 +47,17 @@ fn main() {
         .title("Departments")
         .button("Quit", Cursive::quit),
     );
-
-    siv.run();
 }
 
-fn create_departments_view(siv: &mut Cursive) -> Box<dyn View> {
+fn create_departments_list(s: &mut Cursive) -> Box<dyn View> {
     let mut select_view = SelectView::<String>::new();
-    let departments = siv.user_data::<HashMap<String, Vec<String>>>().unwrap();
-    select_view.add_all_str(departments.keys());
+    let departments = s.user_data::<HashMap<String, Vec<String>>>().unwrap();
+    let mut department_names: Vec<String> = Vec::new();
+    for (k, _v) in departments {
+        department_names.push(k.clone());
+    }
+    department_names.sort();
+    select_view.add_all_str(department_names);
     let departments_view = select_view
         .on_submit(show_department)
         .with_name("departments")
@@ -59,9 +67,72 @@ fn create_departments_view(siv: &mut Cursive) -> Box<dyn View> {
     Box::new(departments_view)
 }
 
-fn show_department(s: &mut Cursive, department: &str) {}
+fn create_employees_view(s: &mut Cursive, department_name: &str) -> Box<dyn View> {
+    let mut select_view = SelectView::<String>::new();
+    let departments = s.user_data::<HashMap<String, Vec<String>>>().unwrap();
+    select_view.add_all_str(departments.get(department_name).unwrap());
+    let employees_view = select_view
+        .on_submit(show_department)
+        .scrollable()
+        .fixed_size((40, 10));
+
+    let employee_buttons = LinearLayout::vertical()
+        .child(Button::new("Add new", create_employee))
+        .child(Button::new("Remove", remove_employee));
+
+    let layout = LinearLayout::horizontal()
+        .child(employees_view)
+        .child(DummyView)
+        .child(employee_buttons);
+
+    Box::new(layout)
+}
+
+fn show_department(s: &mut Cursive, department_name: &str) {
+    s.pop_layer();
+    let emp_view = create_employees_view(s, department_name);
+    s.add_layer(
+        Dialog::around(emp_view)
+            .title(format!("Employees of {}", department_name))
+            .button("Back", |s| {
+                s.pop_layer();
+                create_departments_view(s)
+            }),
+    );
+}
 
 fn create_department(s: &mut Cursive) {
+    fn ok(s: &mut Cursive, department_name: &str) {
+        s.with_user_data(|departments: &mut HashMap<String, Vec<String>>| {
+            departments.insert(String::from(department_name), Vec::new());
+        });
+        s.pop_layer();
+        create_departments_view(s);
+    }
+    s.pop_layer();
+    s.add_layer(
+        Dialog::around(
+            EditView::new()
+                .on_submit(ok)
+                .with_name("name")
+                .fixed_width(40),
+        )
+        .title("Create department")
+        .button("Ok", |s| {
+            let name = s
+                .call_on_name("name", |view: &mut EditView| view.get_content())
+                .unwrap();
+            ok(s, &name);
+        })
+        .button("Cancel", |s| {
+            s.pop_layer();
+        }),
+    );
+}
+
+fn remove_department(s: &mut Cursive) {}
+
+fn create_employee(s: &mut Cursive) {
     fn ok(s: &mut Cursive, name: &str) {
         s.call_on_name("departments", |view: &mut SelectView<String>| {
             view.add_item_str(name)
@@ -88,4 +159,4 @@ fn create_department(s: &mut Cursive) {
     );
 }
 
-fn remove_department(s: &mut Cursive) {}
+fn remove_employee(s: &mut Cursive) {}
