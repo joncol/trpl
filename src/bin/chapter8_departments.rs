ecd382d409edab677dbb6aefd515ea15d2cae0c5
Jonas Collberg <jonas.collberg@gmail.com>
2020-08-01 21:23:05 +0200

    Use user-data in cursive

--- src/bin/chapter8_departments.rs
+++ src/bin/chapter8_departments.rs
@@ -1,21 +1,41 @@
 use cursive::traits::*;
-use cursive::views::{
-    Button, Dialog, DummyView, EditView, LinearLayout, SelectView,
-};
+use cursive::views::{Button, Dialog, DummyView, EditView, LinearLayout, SelectView};
 use cursive::Cursive;
+use std::collections::HashMap;
 
 fn main() {
     let mut siv = cursive::default();
+
     siv.add_global_callback('q', Cursive::quit);
+
+    let mut departments: HashMap<String, Vec<String>> = HashMap::new();
+    departments.insert(
+        String::from("Engineering"),
+        vec!["Emily", "Ethan", "Evie", "Eva", "Erin"]
+            .iter()
+            .map(|s: &&str| s.to_string())
+            .collect(),
+    );
+    departments.insert(
+        String::from("Sales"),
+        vec!["Sophie", "Scarlett", "Samuel", "Sienna", "Sebastian"]
+            .iter()
+            .map(|s: &&str| s.to_string())
+            .collect(),
+    );
+
+    siv.set_user_data(departments);
 
     let department_buttons = LinearLayout::vertical()
         .child(Button::new("Create new", create_department))
         .child(Button::new("Remove", remove_department));
 
+    let departments_view = create_departments_view(&mut siv);
+
     siv.add_layer(
         Dialog::around(
             LinearLayout::horizontal()
-                .child(departments_view())
+                .child(departments_view)
                 .child(DummyView)
                 .child(department_buttons),
         )
@@ -26,15 +46,10 @@ fn main() {
     siv.run();
 }
 
-fn departments_view() -> Box<dyn View> {
+fn create_departments_view(siv: &mut Cursive) -> Box<dyn View> {
     let mut select_view = SelectView::<String>::new();
-    select_view.add_all_str(
-        [
-            String::from("Engineering"),
-            String::from("Sales"),
-        ]
-        .iter(),
-    );
+    let departments = siv.user_data::<HashMap<String, Vec<String>>>().unwrap();
+    select_view.add_all_str(departments.keys());
     let departments_view = select_view
         .on_submit(show_department)
         .with_name("departments")
